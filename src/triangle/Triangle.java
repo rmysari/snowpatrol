60e461e8ed0a97716790e694bcd54790eacddd8d
Rohit Kumar <Rohit Kumar@153.104.58.243>
2013-10-23 15:21:17 -0400

    Sample code

---
+++ src/triangle/Triangle.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package triangle;
+
+/**
+ *
+ * @author Rohit Kumar
+ */
+public class Triangle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Triangle t= new Triangle();
+        t.triangle();
+    }
+    public void triangle()
+    {
+        for(int i=0;i<5;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
